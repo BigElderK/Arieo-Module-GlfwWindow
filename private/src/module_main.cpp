7c792efa1b47e49d6f5a0a32e8195ee21bad19f5
BigElderK <BigElderK@divineland.org>
2026-02-28 07:25:55 +0800

    Refactor interface/module system: add instance, interop and ct_check to Core; update all interfaces and module implementations; update codegen template and build config; add git helper scripts

--- private/src/module_main.cpp
+++ private/src/module_main.cpp
@@ -16,7 +16,7 @@ namespace Arieo
             {
                 glfw_window_manager->initialize();
                 
-                Base::Interface<Interface::Main::IMainModule> main_module = Core::ModuleManager::getInterface<Interface::Main::IMainModule>();
+                Base::Interop<Interface::Main::IMainModule> main_module = Core::ModuleManager::getInterface<Interface::Main::IMainModule>();
                 main_module->registerTickable(glfw_window_manager.queryInterface<Interface::Main::ITickable>());
 
                 Core::ModuleManager::registerInstance<Interface::Window::IWindowManager, GLFWindowManager>(
