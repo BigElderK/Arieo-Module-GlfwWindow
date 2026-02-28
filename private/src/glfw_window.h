11f71fe322265c7ceb3013537431f856f5d0c303
BigElderK <BigElderK@divineland.org>
2026-02-28 14:31:44 +0800

    Relocate base/interface/ headers to base/interop/ and update all includes
    - Move instance.h, interface.h, interop.h from base/interface/ to base/interop/
    - Update include paths in prerequisites.h, module.h, and codegen template
    - Update includes across all interface modules (Archive, Sample, Script)
    - Update includes across all implementation modules (Main, Vulkan, Window backends)

--- private/src/glfw_window.h
+++ private/src/glfw_window.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "interface/window/window.h"
-#include "base/interface/interface.h"
+#include "base/interop/interface.h"
 
 // #define GLFW_INCLUDE_VULKAN
 #define GLFW_INCLUDE_NONE
